ae1c2c65e961bc2ead4252d76b28138b881dd0e9
Nick Brown <overflow2341313@gmail.com>
2019-10-04 12:32:32 -0500

    Update for Django 2.0+

--- admin_export/urls.py
+++ admin_export/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import path, re_path
 from django.contrib.admin.views.decorators import staff_member_required
 from .views import AdminExport
 
 view = staff_member_required(AdminExport.as_view())
-urlpatterns = patterns('',
-   url(r'^export/$', view, name="export"),
-   (r'^export_to_xls/$', view),  # compatibility for users who upgrade without touching URLs
-)
+urlpatterns = [
+   path('export/', view, name="export"),
+   path('export_to_xls/', view),  # compatibility for users who upgrade without touching URLs
+]
