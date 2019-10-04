8873dc20352fb1a548f6273f0607472b065192d2
Nick Brown <overflow2341313@gmail.com>
2019-10-04 12:44:35 -0500

    Update urls.py

--- admin_export/urls.py
+++ admin_export/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, re_path
 from django.contrib.admin.views.decorators import staff_member_required
 from .views import AdminExport
 
+app_name = 'admin_export'
+
 view = staff_member_required(AdminExport.as_view())
 urlpatterns = [
    path('export/', view, name="export"),
